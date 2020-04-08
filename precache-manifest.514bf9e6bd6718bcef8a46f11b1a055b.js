6294c17c44aba9e75a9b83c95815f576fd53128a
Nazar Pyhlyuk <58308716+drake0770@users.noreply.github.com>
2020-04-08 22:55:21 +0300

    Updates

---
+++ precache-manifest.514bf9e6bd6718bcef8a46f11b1a055b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83a46f1f0a1a0b0d5463078022e5827",
+    "url": "/mystore/index.html"
+  },
+  {
+    "revision": "2eb138cba2d96f9d1df3",
+    "url": "/mystore/static/css/main.a76f0b5b.chunk.css"
+  },
+  {
+    "revision": "b55e22dbc81377501d3c",
+    "url": "/mystore/static/js/2.46f031cd.chunk.js"
+  },
+  {
+    "revision": "2d1f0ed21c5c95e3a2cb212dbd2653e2",
+    "url": "/mystore/static/js/2.46f031cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eb138cba2d96f9d1df3",
+    "url": "/mystore/static/js/main.e3cb02c1.chunk.js"
+  },
+  {
+    "revision": "e5765e181cb760452330",
+    "url": "/mystore/static/js/runtime-main.ffda80d5.js"
+  },
+  {
+    "revision": "fb14229720dc11d2b781db5b2e7f6bb3",
+    "url": "/mystore/static/media/admin.fb142297.jpg"
+  },
+  {
+    "revision": "8cc70b8a522d045c8d2b06c97992a96b",
+    "url": "/mystore/static/media/airpods.8cc70b8a.jpg"
+  },
+  {
+    "revision": "f16116dbd449d3d4eef306cee700cfaf",
+    "url": "/mystore/static/media/airpodspro.f16116db.jpg"
+  },
+  {
+    "revision": "07e2b81e19e9a657d1698d164852613e",
+    "url": "/mystore/static/media/imac.07e2b81e.jpg"
+  },
+  {
+    "revision": "7b8a7a6e2e5474bcb63627517ce120d7",
+    "url": "/mystore/static/media/imaccustom.7b8a7a6e.jpg"
+  },
+  {
+    "revision": "9470fa9b75023290ce3e5b24a5b92c34",
+    "url": "/mystore/static/media/ipad19.9470fa9b.jpg"
+  },
+  {
+    "revision": "1d8368b3536a6ef3e8210b033353c657",
+    "url": "/mystore/static/media/ipadpro.1d8368b3.jpg"
+  },
+  {
+    "revision": "f357771915aa9752669aece596b6874a",
+    "url": "/mystore/static/media/iphone11.f3577719.jpeg"
+  },
+  {
+    "revision": "f20a4d715dc1fbc05df11e9efa2aa1f2",
+    "url": "/mystore/static/media/iphone11progold.f20a4d71.jpeg"
+  },
+  {
+    "revision": "23521fc8fef7863efb18b6f8ba3226d0",
+    "url": "/mystore/static/media/iphone11progray.23521fc8.jpeg"
+  },
+  {
+    "revision": "96b8b6658d16c99a7c591909a3352b14",
+    "url": "/mystore/static/media/iphone11prowhite.96b8b665.jpeg"
+  },
+  {
+    "revision": "1d36079df6cf0b3116af79f178db5e77",
+    "url": "/mystore/static/media/iphone7plusgold.1d36079d.jpg"
+  },
+  {
+    "revision": "2eb8a4472e3ab68da9367d429b33ec79",
+    "url": "/mystore/static/media/iphone7silver.2eb8a447.jpg"
+  },
+  {
+    "revision": "e65cd29ac2338bdb30f5c2743a44232a",
+    "url": "/mystore/static/media/iphone8white.e65cd29a.jpeg"
+  },
+  {
+    "revision": "31dadabf796c4935e93ebbd57084ed1f",
+    "url": "/mystore/static/media/iphonexwhite.31dadabf.jpeg"
+  },
+  {
+    "revision": "b1569a9bb2237eec7f0921cf2f5d124e",
+    "url": "/mystore/static/media/logo.b1569a9b.PNG"
+  },
+  {
+    "revision": "4530d58d1470501c71d61bd5a8a18bab",
+    "url": "/mystore/static/media/macair.4530d58d.jpg"
+  },
+  {
+    "revision": "7959663d35fb02316a91a753ccb1d601",
+    "url": "/mystore/static/media/macpro.7959663d.jpg"
+  },
+  {
+    "revision": "8bb64879db55868289f4a114b4e3cf33",
+    "url": "/mystore/static/media/macpro2.8bb64879.jpg"
+  },
+  {
+    "revision": "1b742b222511327a2493d2d2c2bd1731",
+    "url": "/mystore/static/media/usbcable.1b742b22.jpg"
+  },
+  {
+    "revision": "ac2044521ebc7d1bc3cbd2b4dcaaacd9",
+    "url": "/mystore/static/media/watch3.ac204452.jpg"
+  }
+]);
